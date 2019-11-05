05016d5c098206ff837cdb9c3811df5a89822250
Tom Van Dijk <129102@dr.nassaucollege.nl>
2019-11-05 15:31:18 +0100

    Create bot

---
+++ commands/say.js
@@ -0,0 +1,29 @@
+const commands = require("bananenbase").command;
+
+module.exports = class say extends commands {
+  constructor(client) {
+    super(client, {
+      name: "say",
+      description: "Let the bot say something",
+      category: "Owner",
+      subCommands: ["s"],
+      args: ["What the bots should say : required"]
+    }, {
+      permLevel : 1, 
+      permissions: { 
+        me: ["SEND_MESSAGES"], 
+        user: ["SEND_MESSAGES"] 
+      }
+    });
+  }
+
+    async run(msg, args) {
+        if (!args) return msg.channel.send("Wat wil je dat de bot gaat zeggen?")
+        try{
+          let say = args.join("")
+          msg.channel.say(say)
+          await msg.delete()
+        }
+    }
+  
+  }
---
+++ index.js
@@ -0,0 +1,33 @@
+// Requireing the package
+const BananenBase = require("bananenbase");
+
+new BananenBase({
+  token: "TOKEN", 
+  database: {
+    package: "json", 
+    name: ".tmp_database.json"
+  },
+  language: "NL",
+  bot: (client) => {
+    console.log("-/READY\-")
+  },
+  settings: true,
+  botConfig: __dirname + "config.json",
+  prefix: "solli!", 
+  ignore: { 
+    bot: true, 
+    pm: false 
+  },
+  consoleFunctions: true,
+  permissionLevels: [ 
+    (client, message, args) => {
+      return true; 
+    },
+    (client, message, args) => { 
+      if (client.config.botOwners.includes(message.author.id)) return true; 
+      return false; 
+    }
+  ],
+  pmPrefix: false, 
+  webPort: 80 
+});
