62adda83aa552218becbbdb6901421348564ed19
Tom Van Dijk <129102@dr.nassaucollege.nl>
2019-11-11 10:51:41 +0100

    Create lenny.js

---
+++ commands/lenny.js
@@ -0,0 +1,23 @@
+const commands = require("bananenbase").command;
+
+module.exports = class say extends commands {
+  constructor(client) {
+    super(client, {
+      name: "lenny",
+      description: "Wat zeg je, lenny?",
+      category: "fun",
+      subCommands: ["lenni"]
+    }, {
+      permLevel : 0, 
+      permissions: { 
+        me: ["SEND_MESSAGES"], 
+        user: ["SEND_MESSAGES"] 
+      }
+    });
+  }
+
+    async run(msg, args) {
+        let lennyArray = ["( ͡° ͜ʖ ͡°)", "(づ｡◕‿‿◕｡)づ", "┬┴┬┴┤° ͜ʖ ͡°) ├┬┴┬┴", "(ง'̀-'́)ง", "(づ￣ ³￣)づ", "◉_◉", "( ͡ᵔ ͜ʖ ͡ᵔ )", "༼ʘ̚ل͜ʘ̚༽", "(ʘ‿ʘ)", "(✿'‿`)"]
+    }
+  
+  }
