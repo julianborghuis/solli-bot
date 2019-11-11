2c83734d3f88e85f94bb5b479e81fbeb8eea86c3
Tom Van Dijk <129102@dr.nassaucollege.nl>
2019-11-11 11:22:51 +0100

    Update lenny.js

--- commands/lenny.js
+++ commands/lenny.js
@@ -18,6 +18,20 @@ module.exports = class say extends commands {
 
     async run(msg, args) {
         let lennyArray = ["( ͡° ͜ʖ ͡°)", "(づ｡◕‿‿◕｡)づ", "┬┴┬┴┤° ͜ʖ ͡°) ├┬┴┬┴", "(ง'̀-'́)ง", "(づ￣ ³￣)づ", "◉_◉", "( ͡ᵔ ͜ʖ ͡ᵔ )", "༼ʘ̚ل͜ʘ̚༽", "(ʘ‿ʘ)", "(✿'‿`)", ">_>"]
-    }
+        let chosen = Math.floor(Math.random() * Math.floor(lennyArray.length - 1));
+        if(!await this.client.db.get("lastLenny") { 
+          await this.client.db.set("lastLenny", chosen)
+          return msg.channel.send(lennyArray[chosen];
+        } else{
+          let previousChosen = this.client.db.get("lastLenny")
+          if (previousChosen == chosen) {
+            this.run(msg, args);
+            return;
+          } else {
+            await this.client.db.set("lastLenny", chosen)
+            return msg.channel.send(lennyArray[chosen];
+          }
+        }
+      }
   
   }
